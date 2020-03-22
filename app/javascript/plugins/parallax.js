888b5692ae3ec7bc6d1908b20d40dc322d29aa15
Sofia Stephenson <sgandsteph@gmail.com>
2020-03-21 22:49:22 -0700

    Replaces missing website photos and give them opaque transition

---
+++ app/javascript/plugins/parallax.js
@@ -0,0 +1,8 @@
+import simpleParallax from 'simple-parallax-js';
+
+    // var image = document.getElementsByClassName('tastebudd');
+    // new simpleParallax(image);
+// import rallax from 'rallax.js';
+
+// const target = document.querySelector('.tastebudd')
+// const parallax = rallax(target, { speed: 0.4 })
